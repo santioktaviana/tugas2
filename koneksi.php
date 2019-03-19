eaeb79f6192f280d7900a0fc4f68b19880077160
santioktaviana <41132917+santioktaviana@users.noreply.github.com>
2019-03-19 13:24:48 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    
+    $kon = mysqli_connect("localhost", "id7899352_santi","santioktaviana22","id7899352_akademik");
+    if (mysqli_connect_errno()) {
+        echo "Koneksi database gagal : " .mysqli_connect_error();
+    }
+?>
